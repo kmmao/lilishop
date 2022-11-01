e7b7ae2df7b5274ea0d5c358731e8dd77ce293e8
liukai <liukai@quanwu.info>
2022-11-01 12:01:37 +0800

    修正Sql问题

--- framework/src/main/java/cn/lili/modules/ddg/mapper/DdgParentsAssignGoodsSkuMapper.java
+++ framework/src/main/java/cn/lili/modules/ddg/mapper/DdgParentsAssignGoodsSkuMapper.java
@@ -24,6 +24,6 @@ public interface DdgParentsAssignGoodsSkuMapper extends BaseMapper<DdgParentsAss
      *
      * @return 商品SKU列表
      */
-    @Select("SELECT gs.* FROM li_goods_sku gs WHERE gs.id IN(SELECT goods_sku_id FROM ddg_parents_assign_goods_sku WHERE status=1 AND child_id=SELECT gs.*,dpags.is_gold_pay FROM ddg_parents_assign_goods_sku dpags LEFT JOIN  li_goods_sku gs ON gs.id = dpags.goods_sku_id WHERE dpags.`status` = 1 AND dpags.child_id = ${childId} ${ew.customSqlSegment}")
+    @Select("SELECT gs.*,dpags.is_gold_pay FROM ddg_parents_assign_goods_sku dpags LEFT JOIN  li_goods_sku gs ON gs.id = dpags.goods_sku_id WHERE dpags.`status` = 1 AND dpags.child_id = ${childId} ${ew.customSqlSegment}")
     IPage<GoodsSkuVO> getGoodsSkuByChildIdFormAssign(IPage<GoodsSkuVO> page, @Param(Constants.WRAPPER) Wrapper<GoodsSkuVO> queryWrapper, String childId);
 }
